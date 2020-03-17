7d752a6ca4c95bac42b544dba685f9e615bfad2a
Will <seoktaehyeon@msn.com>
2020-03-17 18:32:43 +0800

    update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 if [[ -z "${TESTLINK_URL}" ]]; then
-    export TESTLINK_URL=''
+    echo "Need provide TESTLINK_URL"
+    exit 1
 fi
 if [[ -z "${TESTLINK_USER}" ]]; then
+    echo "Need provide TESTLINK_USER"
+    exit 1
     export TESTLINK_USER=''
 fi
 if [[ -z "${TESTLINK_DEVKEY}" ]]; then
-    export TESTLINK_DEVKEY=''
+    echo "Need provide TESTLINK_DEVKEY"
+    exit 1
 fi
 if [[ -z "${ISSUE_TRACKER_URI_VIEW}" ]]; then
-    export ISSUE_TRACKER_URI_VIEW=''
+    echo "Need provide ISSUE_TRACKER_URI_VIEW"
+    exit 1
 fi
 export PYTHONPATH=$PWD
 python3 metrics/run.py
